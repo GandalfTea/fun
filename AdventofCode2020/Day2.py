5ea32a134f99d31217e6e43f42ea9b65356ac837
GandalfTea <58654842+GandalfTea@users.noreply.github.com>
2020-12-02 16:13:36 +0000

    Add files via upload

--- AdventofCode2020/Day2.py
+++ AdventofCode2020/Day2.py
@@ -1,4 +1,4 @@
-input = ["1-13 r: gqdrspndrpsrjfjx
+input = """1-13 r: gqdrspndrpsrjfjx
 5-16 j: jjjjkjjzjjjjjfjzjjj
 14-16 r: rrrnrrrrrcnrgxrr
 1-3 k: bkktwhgktv
@@ -997,4 +997,36 @@ input = ["1-13 r: gqdrspndrpsrjfjx
 4-6 g: kggggg
 6-7 c: cccccdqcc
 2-6 x: vjkxbrfwnj
-16-18 s: kssssssswssssssssssb"]
+16-18 s: kssssssswssssssssssb"""
+
+
+pri = input.split("\n")
+
+answer = 0
+answer1 = 0
+for pas in pri:
+    a = pas.split(None)
+    temp = a[0].split("-", 1)
+    num1 = int(temp[0])
+    num2 = int(temp[1])
+    
+    letter = a[1].split(":", 1)[0]
+    
+    # Prob 1
+    f = 0
+    for let in a[2]:
+        if let == letter:
+            f +=1
+    if f >= num1 and f <= num2:
+        answer += 1
+    
+    # Prob 2
+    if a[2][num1 - 1 ] == letter or a[2][num2 - 1] == letter:
+        if a[2][num1 - 1] != a[2][num2 - 1]:
+            #print(letter, " : ", a[2][num1 - 1], a[2][num2 - 1])
+            answer1 += 1
+
+
+print(answer)
+print(answer1)
+
